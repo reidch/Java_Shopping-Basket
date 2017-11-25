ae9bd3460523964fc1b236f340900db92217680f
Caroline Reid <carolinehreid@gmail.com>
2017-11-25 14:47:38 +0000

    adds item class and tests

--- ShoppingBasket/app/src/main/java/com/example/carolinereid/shoppingbasket/Item.java
+++ ShoppingBasket/app/src/main/java/com/example/carolinereid/shoppingbasket/Item.java
@@ -5,4 +5,31 @@ package com.example.carolinereid.shoppingbasket;
  */
 
 public class Item {
+    private String description;
+    private String brand;
+    private double price;
+//    bogofStatus
+
+
+    public Item(String description, String brand, double price) {
+        this.description = description;
+        this.brand = brand;
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
--- ShoppingBasket/app/src/test/java/com/example/carolinereid/shoppingbasket/TestItem.java
+++ ShoppingBasket/app/src/test/java/com/example/carolinereid/shoppingbasket/TestItem.java
@@ -1,8 +1,48 @@
 package com.example.carolinereid.shoppingbasket;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by carolinereid on 25/11/2017.
  */
 
 public class TestItem {
+    Item item;
+    Item item1;
+    Item item2;
+    Item item3;
+    Item item4;
+
+    @Before
+    public void before() {
+        item = new Item("Crema e gusto espresso", "Lavazza", 4.26);
+        item1 = new Item("semi-skimmed milk, 1 litre", "Grahams", 0.79);
+        item2 = new Item("demerara sugar", "Tate and Lyle", 1.25);
+        item3 = new Item("Choco Leibniz biscuits", "Bahlsen", 1.58);
+        item4 = new Item("dark chocolate gingers", "Border biscuits", 1.00);
+    }
+
+    @Test
+    public void hasDescription() {
+        assertEquals("dark chocolate gingers", item4.getDescription());
+    }
+
+    @Test
+    public void hasBrand() {
+        assertEquals("Bahlsen", item3.getBrand());
+    }
+
+    @Test
+    public void hasPrice() {
+        assertEquals(0.79, item1.getPrice(), 0.1);
+    }
+
+    @Test
+    public void canChangePrice() {
+        item.setPrice(4.58);
+        assertEquals(4.58, item.getPrice(), 0.1);
+    }
 }
